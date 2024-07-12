544f50911cd4f936793bc1da96b080048f863ed8
ygdhpf1994 <1270253125@qq.com>
2024-07-12 18:17:45 +0800

    sds"

--- src/pages/Demo-suipian.vue
+++ src/pages/Demo-suipian.vue
@@ -41,16 +41,86 @@ onMounted(() => {
     sceneView.add(axesHelper);
 
     // sceneView.add(mesh);
-    const arr = createPoint();
+    const {arr, centerList} = createPoint();
     console.log(sceneView, arr);
     const fgsContainer = new THREE.Group();
     sceneView.add(fgsContainer);
     fgsContainer.position.copy(new THREE.Vector3(-36, -36, 0.1));
-    arr.forEach((item) => {
+    const vertexShader = /*glsl*/ ` 
+        uniform float iTime;
+        uniform float uTime;
+        uniform vec2 uMouse;
+        uniform vec2 iMouse;
+        uniform vec2 iResolution;
+        uniform float uRandom;
+        uniform float uLayerId;
+        uniform vec3 uCenter;
+
+        mat2 rotation2d(float angle){
+            float s=sin(angle);
+            float c=cos(angle);
+            
+            return mat2(
+                c,-s,
+                s,c
+            );
+        }
+        
+        mat4 rotation3d(vec3 axis,float angle){
+            axis=normalize(axis);
+            float s=sin(angle);
+            float c=cos(angle);
+            float oc=1.-c;
+            
+            return mat4(
+                oc*axis.x*axis.x+c,oc*axis.x*axis.y-axis.z*s,oc*axis.z*axis.x+axis.y*s,0.,
+                oc*axis.x*axis.y+axis.z*s,oc*axis.y*axis.y+c,oc*axis.y*axis.z-axis.x*s,0.,
+                oc*axis.z*axis.x-axis.y*s,oc*axis.y*axis.z+axis.x*s,oc*axis.z*axis.z+c,0.,
+                0.,0.,0.,1.
+            );
+        }
+        
+        vec2 rotate(vec2 v,float angle){
+            return rotation2d(angle)*v;
+        }
+        vec3 rotate(vec3 v,vec3 axis,float angle){
+            return(rotation3d(axis,angle)*vec4(v,1.)).xyz;
+        }
+        vec3 distort(vec3 p){
+            vec3 tx1=vec3(-uCenter.x,-uCenter.y,-uCenter.z);
+            p+=tx1;
+
+            float angle=iTime*uRandom*0.05;
+            p=rotate(p,vec3(0.,1.,0.),angle);
+
+            vec3 tx2=vec3(uCenter.x,uCenter.y,uCenter.z);
+            p+=tx2;
+
+            //p*=(.6-uLayerId*.5);
+
+            return p;
+        }
+        void main(){
+            vec3 p=position;
+            // vec3 N=normal;
+
+            p=distort(p);
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(p, 1.0);
+        }
+    `;
+    const fragmentShader = `
+        uniform float iTime;
+        void main(){
+            gl_FragColor=vec4(0.2, 0.2,0.2, 1.0);
+        }
+    `;
+    let i = 0.0;
+    const materialList = []
+    arr.forEach((item, index) => {
         const shape = createShape(item);
         const extrudeSettings = {
             steps: 2,
-            depth: 0.1,
+            depth: 0.001,
             bevelEnabled: true,
             bevelThickness: 0.0005,
             bevelSize: 0.0005,
@@ -58,11 +128,44 @@ onMounted(() => {
             bevelSegments: 1,
         };
         const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+        const z = (Math.random() - 0.5) * 20
+        const material = new THREE.ShaderMaterial({
+            side: THREE.DoubleSide,
+            transparent: true,
+            uniforms: {
+                iTime: {
+                    value: 0
+                },
+                uTexture: {
+                    value: 0,
+                },
+                uLightPosition: {
+                    value: new THREE.Vector3(-0.2, -0.2, 3),
+                },
+                uLightColor: {
+                    value: new THREE.Color("#eeeeee"),
+                },
+                uRandom: {
+                    value: THREE.MathUtils.randFloat(0.1, 1.1),
+                },
+                uMouse: {
+                    value: new THREE.Vector2(0.5, 0.5),
+                },
+                uLayerId: {
+                    value: 0,
+                },
+                uCenter: {
+                    value: new THREE.Vector3(centerList[index].x??0, centerList[index].y??0, z)
+                }
+            },
+            vertexShader,
+            fragmentShader,
+        });
+        materialList.push(material)
+        console.log(centerList[index])
         const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.z = (Math.random() - 0.5) * 20
+        mesh.position.z = z;
         fgsContainer.add(mesh);
-        console.log(mesh);
     });
     const fgsContainer2 = new THREE.Group().copy(fgsContainer.clone());
     fgsContainer2.position.y = fgsContainer.position.y - 100;
@@ -70,19 +173,23 @@ onMounted(() => {
     const totalG = new THREE.Group();
     totalG.add(fgsContainer);
     totalG.add(fgsContainer2);
-    sceneView.add(totalG)
-    let i = 0
-    const animate = () =>{
-        i++
-        totalG.position.y = (i * 0.5)
+    sceneView.add(totalG);
+    console.log(totalG)
+
+    const animate = () => {
+        i += 1.0;
+        totalG.position.y = i * 0.2;
         if (totalG.position.y > 100) {
-            i = 0
+            i = 0;
+        }
+        for(let j = 0 ; j < materialList.length; j++) {
+            materialList[j].uniforms.iTime.value = i
         }
         sceneView.renderer.render(sceneView.scene, sceneView.camera);
         sceneView.controls.update();
         requestAnimationFrame(animate);
-    }
-    animate()
+    };
+    animate();
 });
 
 const createPoint = () => {
@@ -91,6 +198,7 @@ const createPoint = () => {
         maxX = 8,
         maxY = 9;
     const polygons = [];
+    const centerList = [];
 
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
@@ -122,6 +230,8 @@ const createPoint = () => {
                 y: 0,
             };
             let angle = THREE.MathUtils.randFloat(0, 2 * Math.PI);
+            const xarr = []
+            const yarr = []
             for (let k = 0; k < edgeCount; k++) {
                 if (k === 0) {
                     firstPoint = {
@@ -129,6 +239,8 @@ const createPoint = () => {
                         y: (j % maxY) * 10,
                     };
                     points.push(firstPoint);
+                    xarr.push(firstPoint.x)
+                    yarr.push(firstPoint.y)
                 } else {
                     // random polar
                     const r = 10;
@@ -138,13 +250,21 @@ const createPoint = () => {
                         y: firstPoint.y + r * Math.sin(angle),
                     };
                     points.push(anotherPoint);
+                    xarr.push(anotherPoint.x)
+                    yarr.push(anotherPoint.y)
                 }
             }
             polygons.push(points);
+            const data = new THREE.Vector2((Math.max(...xarr) + Math.min(...xarr)) / 2, (Math.max(...yarr) + Math.min(...yarr)) / 2)
+            centerList.push(data)
         }
     }
+    console.log(centerList)
 
-    return polygons;
+    return {
+        arr: polygons,
+        centerList
+    };
 };
 
 const createShape = (item) => {
